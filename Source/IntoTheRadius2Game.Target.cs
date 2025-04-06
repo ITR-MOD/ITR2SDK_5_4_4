09dee0709aa95205b773026777278e6c3eb2c291
ChuckSharts <tcollins@flyrightinc.com>
2025-04-06 08:06:22 -0400

    few fixes for cooking
    few fixes for cooking

--- Source/IntoTheRadius2Game.Target.cs
+++ Source/IntoTheRadius2Game.Target.cs
@@ -3,6 +3,7 @@
 public class IntoTheRadius2GameTarget : TargetRules {
 	public IntoTheRadius2GameTarget(TargetInfo Target) : base(Target) {
 		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
 		ExtraModuleNames.AddRange(new string[] {
 			"IntoTheRadius2",
 		});
